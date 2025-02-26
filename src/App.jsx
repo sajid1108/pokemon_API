7942825d3717d476fc4f46cd7d716492c9ecd398
sajid1108 <cahsekolahbelajar@gmail.com>
2025-02-27 03:33:34 +0700

    change color

--- src/App.jsx
+++ src/App.jsx
@@ -44,7 +44,7 @@ function App() {
 
   return (
     <>
-      <div className="App min-h-screen bg-[#ffccbb] flex flex-col items-center p-8">
+      <div className="App min-h-screen bg-[#f5f5dc] flex flex-col items-center p-8">
         {loading ? (
           <div className="flex justify-center items-center h-screen">
             <img src={DualBall} alt="Loading" width="200" height="200" />
