e9be780622e48dc2134218c6dda4ea19bd6a87b4
sajid1108 <cahsekolahbelajar@gmail.com>
2025-03-17 11:52:07 +0700

    update 1.2

--- src/App.jsx
+++ src/App.jsx
@@ -44,7 +44,7 @@ function App() {
 
   return (
     <>
-      <div className="App min-h-screen bg-[#d4b393] flex flex-col items-center p-8">
+      <div className="App min-h-screen bg-[#d4b392] flex flex-col items-center p-8">
         {loading ? (
           <div className="flex justify-center items-center h-screen">
             <img src={DualBall} alt="Loading" width="50" height="50" />
